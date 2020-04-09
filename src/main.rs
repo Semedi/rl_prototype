cc78a099470267eff3d44f17045e15d7b8800da0
Sergio Semedi <sergio.semedi@keepler.io>
2020-04-10 01:20:10 +0200

    basic initialization

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use amethyst::{
+    prelude::*,
+    core::TransformBundle,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+mod rl;
+use crate::rl::Rl;
+
+struct MyState;
+
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let assets_dir = app_root.join("assets");
+
+    // We'll put the rest of the code here.
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()),
+        )?
+        .with_bundle(TransformBundle::new())?;
+
+    let mut game = Application::new(assets_dir, Rl, game_data)?;
+    game.run();
+
+    Ok(())
+}
